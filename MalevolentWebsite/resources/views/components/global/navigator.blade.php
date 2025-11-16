60715657db78310b37d6fc15509df3f7d856db69
Clipstone <declandavidwww@gmail.com>
2025-11-16 10:44:14 +0000

    Add dropdown menus to navigation bar
    Enhanced the navigation bar with dropdown menus for Homepage, Community, Support, and Account sections. Updated CSS to style the new dropdowns and their interactive states, improving navigation and user experience.

--- MalevolentWebsite/resources/views/components/global/navigator.blade.php
+++ MalevolentWebsite/resources/views/components/global/navigator.blade.php
@@ -2,13 +2,43 @@
     <div class="container">
         <div class="navigator">
             <div>
-                <a><li>Homepage <i class="fa-solid fa-angle-up"></i></li></a>
-                <a><li>Community <i class="fa-solid fa-angle-up"></i></li></a>
-                <a><li>Support <i class="fa-solid fa-angle-up"></i></li></a>
+                <div class="menu">
+                    <a><li>Homepage <i class="fa-solid fa-angle-up"></i></li></a>
+                    <div class="menu-content">
+                        <a><li><i class="fa-solid fa-house"></i> Homepage</li></a>
+                        <div class="divider"></div>
+                        <a><li><i class="fa-solid fa-newspaper"></i> Newspaper</li></a>
+                        <a><li><i class="fa-solid fa-music"></i> Radio Station</li></a>
+                    </div>
+                </div>
+                <div class="menu">
+                    <a><li>Community <i class="fa-solid fa-angle-up"></i></li></a>
+                    <div class="menu-content">
+                        <a><li><i class="fa-solid fa-chart-area"></i> Round Leaderboards</li></a>
+                        <a><li><i class="fa-solid fa-chart-pie"></i> Stats Leaderboards</li></a>
+                        <div class="divider"></div>
+                    </div>
+                </div>
+                <div class="menu">
+                    <a><li>Support <i class="fa-solid fa-angle-up"></i></li></a>
+                    <div class="menu-content">
+                        <a><li><i class="fa-solid fa-ticket"></i> Tickets</li></a>
+                        <a><li><i class="fa-solid fa-book"></i> Guides</li></a>
+                        <div class="divider"></div>
+                        <a><li><i class="fa-brands fa-discord"></i> Discord Server</li></a>
+                        <a><li><i class="fa-brands fa-youtube"></i> Youtube Channel</li></a>
+                    </div>
+                </div>
                 <a><li>Store</li></a>
             </div>
             <div>
-                <a><li>Account <i class="fa-solid fa-angle-up"></i></li></a>
+                <div class="menu">
+                    <a><li>Account <i class="fa-solid fa-angle-up"></i></li></a>
+                    <div class="menu-content">
+                        <a><li><i class="fa-solid fa-user-plus"></i> Register</li></a>
+                        <a><li><i class="fa-solid fa-right-to-bracket"></i> Login</li></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
