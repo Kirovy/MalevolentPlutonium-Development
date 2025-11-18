fe628e87c080753bd255b1823a1b1ca281346bd7
ScoobyDooby <declandavidwww@gmail.com>
2025-11-18 19:28:14 +0000

    Add initial Blade templates for multiple pages
    Created basic Blade view files for commands, guides, newspaper, player, radio, round, search, server, store, team, and tickets pages with shared layout components. Also updated PHP IDE configuration to remove 'asDefaultInterpreter' attributes for code analysis tools.

---
+++ MalevolentWebsite/resources/views/team.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <x-global.head/>
+    </head>
+    <body>
+
+        <x-global.background/>
+        <x-global.navigator/>
+        <x-global.header/>
+
+        <div class="container">
+            <div class="content">
+                <div>
+        
+
+
+                </div>
+                <div>
+
+
+
+                </div>
+            </div>
+        </div>
+
+        <x-global.footer/>
+
+    </body>
+</html>
